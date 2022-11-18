64a703fc83ad4536a2ea3dfd0652fcc53a5a2251
chandninigam <51984693+chandninigam@users.noreply.github.com>
2022-11-18 19:59:45 +0530

    Update folder structuring

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,5 @@
-import Footer from "../src/footer/main";
-import Header from "../src/home/main";
+import Footer from "../component/footer/main";
+import Header from "../component/home/main";
 
 export default function Home() {
   return (
