79cf36b3d5ea8d696fd08f70d5b18225f4648940
chandninigam <51984693+chandninigam@users.noreply.github.com>
2022-11-30 20:56:03 +0530

    Improve coding style for navbar ul

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,24 +1,29 @@
 import Link from "next/link";
 import styles from "../styles/Header.module.css";
 
+export interface NavData {
+  text: string;
+  link: string;
+}
+
+const NavTabs: Array<NavData> = [
+  { text: "Home", link: "/" },
+  { text: "Blogs", link: "/blogs" },
+  { text: "About", link: "/about" },
+  { text: "Contact", link: "/contact" },
+];
+
 export default function Header() {
   return (
     <div className={styles.container}>
       <h1 className={styles.heading}>Chandni Nigam</h1>
       <nav className={styles.nav}>
         <ul className={styles.ulHeader}>
-          <li className={styles.liHeader}>
-            <Link href="/">Home</Link>
-          </li>
-          <li className={styles.liHeader}>
-            <Link href="/blogs">Blogs</Link>
-          </li>
-          <li className={styles.liHeader}>
-            <Link href="/about">About</Link>
-          </li>
-          <li className={styles.liHeader}>
-            <Link href="/contact">Contact</Link>
-          </li>
+          {NavTabs.map((tab) => (
+            <li className={styles.liHeader}>
+              <Link href={tab.link}>{tab.text}</Link>
+            </li>
+          ))}
         </ul>
       </nav>
     </div>
