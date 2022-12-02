89864c9a478a89e58d4a949a42af6f9b6635e400
chandninigam <51984693+chandninigam@users.noreply.github.com>
2022-12-02 20:15:15 +0530

    Added About and Contact page (#9)

---
+++ src/pages/about.tsx
@@ -0,0 +1,13 @@
+import Container from "../components/container";
+import Footer from "../components/footer";
+import Header from "../components/header";
+
+export default function About() {
+  return (
+    <Container>
+      <Header />
+      <div className="main-container">About main container</div>
+      <Footer />
+    </Container>
+  );
+}
---
+++ src/pages/contact.tsx
@@ -0,0 +1,13 @@
+import Container from "../components/container";
+import Footer from "../components/footer";
+import Header from "../components/header";
+
+export default function Contact() {
+  return (
+    <Container>
+      <Header />
+      <div className="main-container">Contact main container</div>
+      <Footer />
+    </Container>
+  );
+}
