3b3976cfc0c460dc18ec31275f8e06bc23a35cc6
chandninigam <51984693+chandninigam@users.noreply.github.com>
2022-12-09 23:04:33 +0530

    Added About Section (#14)

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,13 +1,38 @@
+import { GetStaticPathsContext } from "next";
 import Container from "../components/container";
 import Footer from "../components/footer";
 import Header from "../components/header";
+import path from "path";
+import fs from "fs";
+import matter from "gray-matter";
+import { marked } from "marked";
 
-export default function About() {
+interface IAboutProps {
+  content: string;
+}
+
+export default function About(props: IAboutProps) {
   return (
     <Container>
       <Header />
-      <div className="main-container">About main container</div>
+      <div
+        className="main-container"
+        dangerouslySetInnerHTML={{
+          __html: props.content,
+        }}
+      ></div>
       <Footer />
     </Container>
   );
+}
+
+export async function getStaticProps(ctx: GetStaticPathsContext) {
+  const aboutContentPath = path.join("content");
+  const aboutContent = fs.readFileSync(aboutContentPath + "/About.md", "utf-8");
+  const { content } = matter(aboutContent);
+  return {
+    props: {
+      content: marked(content),
+    },
+  };
 }
