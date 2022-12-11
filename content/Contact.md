a4036797c81d6d4e57478bbc6a0e811ffb3a4d7c
chandninigam <51984693+chandninigam@users.noreply.github.com>
2022-12-11 15:31:15 +0530

    Added Contact Page

---
+++ content/Contact.md
@@ -0,0 +1,13 @@
+---
+title: My First Item
+id: 1
+date: 28-11-2022
+---
+
+You can connect with me
+
+- <a href="#">Github </a>
+- <a href="#">LinkedIn </a>
+- <a href="#">Twitter </a>
+
+And can drop mail on nigamchandni139@gmail.com
--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -1,13 +1,42 @@
+import { GetStaticPathsContext } from "next";
 import Container from "../components/container";
 import Footer from "../components/footer";
 import Header from "../components/header";
+import path from "path";
+import fs from "fs";
+import matter from "gray-matter";
+import { marked } from "marked";
+import styles from "../styles/Contact.module.css";
 
-export default function Contact() {
+interface IContactProps {
+  content: string;
+}
+
+export default function Contact(props: IContactProps) {
   return (
     <Container>
       <Header />
-      <div className="main-container">Contact main container</div>
+      <div
+        className={styles.container}
+        dangerouslySetInnerHTML={{
+          __html: props.content,
+        }}
+      ></div>
       <Footer />
     </Container>
   );
+}
+
+export async function getStaticProps(ctx: GetStaticPathsContext) {
+  const contactContentPath = path.join("content");
+  const contactContent = fs.readFileSync(
+    contactContentPath + "/Contact.md",
+    "utf-8"
+  );
+  const { content } = matter(contactContent);
+  return {
+    props: {
+      content: marked(content),
+    },
+  };
 }
