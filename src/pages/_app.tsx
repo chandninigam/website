4eac3b219273815752a05e13733137b52387a96d
chandninigam <51984693+chandninigam@users.noreply.github.com>
2022-12-13 14:25:46 +0530

    Added Custom Font (#21)

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,18 @@
 import "../styles/globals.css";
+import { Roboto } from "@next/font/google";
+
+const roboto = Roboto({
+  subsets: ["latin"],
+  weight: ["400", "700"],
+});
 
 function MyApp({ Component, pageProps }: any) {
   console.log("appp execurted");
-  return <Component {...pageProps} />;
+  return (
+    <main className={roboto.className}>
+      <Component {...pageProps} />;
+    </main>
+  );
 }
 
 export default MyApp;
