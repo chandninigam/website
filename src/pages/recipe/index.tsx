5d690e9e8cee0cddeee2443f1093785449afc992
chandni nigam <nigamchandni139@gmail.com>
2023-01-23 11:41:26 +0530

    Consistency in styling

--- src/pages/recipe/index.tsx
+++ src/pages/recipe/index.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 import Container from "../../components/container";
 import Footer from "../../components/footer";
 import Header from "../../components/header";
-import styles from "../../styles/Recipe.module.css";
+import styles from "../../styles/Recipes.module.css";
 
 export default function Recipes() {
   return (
