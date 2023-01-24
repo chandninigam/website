db1b4613a15fe46773895d82aefc75058c1a6ea4
chandninigam <51984693+chandninigam@users.noreply.github.com>
2023-01-24 17:22:23 +0530

    Added second recipe (#31)

--- src/pages/recipe/index.tsx
+++ src/pages/recipe/index.tsx
@@ -1,8 +1,13 @@
+import matter from "gray-matter";
+import { GetStaticPropsContext } from "next";
 import Link from "next/link";
 import Container from "../../components/container";
 import Footer from "../../components/footer";
 import Header from "../../components/header";
 import styles from "../../styles/Recipes.module.css";
+import fs from "fs";
+import { marked } from "marked";
+import path from "path";
 
 export default function Recipes() {
   return (
@@ -14,6 +19,11 @@ export default function Recipes() {
           <li>
             <Link href="/recipe/maggie-nutrients" className="recipe-link">
               Maggie With Full Nutrients
+            </Link>
+          </li>
+          <li>
+            <Link href="/recipe/mixed-veg-soup" className="recipe-link">
+              Mixed Veg Soup
             </Link>
           </li>
         </ul>
