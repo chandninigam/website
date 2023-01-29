d0bf1554a56ec986134cdec2d98f0029775c3bc8
chandni nigam <nigamchandni139@gmail.com>
2023-01-29 21:21:46 +0530

    Added Dynamic Linking

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,14 @@
 import "../styles/globals.css";
-import { Roboto } from "@next/font/google";
+import { Source_Sans_Pro } from "@next/font/google";
 
-const roboto = Roboto({
+const font = Source_Sans_Pro({
   subsets: ["latin"],
   weight: ["400", "700"],
 });
 
 function MyApp({ Component, pageProps }: any) {
   return (
-    <main className={roboto.className}>
+    <main className={font.className}>
       <Component {...pageProps} />;
     </main>
   );
